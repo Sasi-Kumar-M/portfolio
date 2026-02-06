d28fb9afeb87a2366fc989997273a41039677d9c
Sasi Kumar <sasikumar0107@gmail.com>
2026-02-06 10:17:21 +0530

    Refactor service worker for cache management
    Updated service worker to use new cache version and simplified asset caching.

--- service-worker.js
+++ service-worker.js
@@ -1,20 +1,7 @@
-const CACHE_NAME="sasi-portfolio-v2";
-const ASSETS=["./","./index.html","./style.css","./script.js","./manifest.json","./assets/profile.jpg"];
-
+const CACHE="v3";
 self.addEventListener("install",e=>{
-self.skipWaiting();
-e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(ASSETS)));
+ e.waitUntil(caches.open(CACHE).then(c=>c.addAll(["./"])));
 });
-
-self.addEventListener("activate",e=>{
-e.waitUntil(caches.keys().then(k=>Promise.all(k.filter(x=>x!==CACHE_NAME).map(x=>caches.delete(x)))));
-self.clients.claim();
-});
-
 self.addEventListener("fetch",e=>{
-e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));
-});
-
-self.addEventListener("message",e=>{
-if(e.data==="SKIP_WAITING") self.skipWaiting();
+ e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));
 });
